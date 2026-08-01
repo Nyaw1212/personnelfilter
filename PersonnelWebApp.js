ab2b97a71633bee3c73496b5377c4b033cf1389a
Nyaw1212 <alvin.chiao@mayersolar.com>
2026-08-01 04:40:45 -0500

    Load retry safety after report generation bridge

--- PersonnelWebApp.js
+++ PersonnelWebApp.js
@@ -62,6 +62,7 @@ function includePlatformBundle() {
     "PlatformReportEngineJS",
     "PlatformReportPreviewUIJS",
     "PlatformReportGenerationBridgeJS",
+    "ReportGenerationRetrySafetyJS",
     "ReportToastDedupFixJS",
     "ReassignmentPluginV1JS",
     "ReassignmentPluginV1GroupChangeFixJS",
